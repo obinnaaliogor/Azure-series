2234b6968f54118ea8d56ecb0b36f6055bb1a787
obinnaaliogor <wiz.obi7509@gmail.com>
2024-03-31 17:17:33 +0200

    csi driver and azure key vault

--- Day-22/01-create-function-app.sh
+++ Day-22/01-create-function-app.sh
@@ -22,3 +22,4 @@ az storage account create --name $storage --location "$location" --resource-grou
 # Create a serverless python function app in the resource group.
 echo "Creating $functionApp"
 az functionapp create --name $functionApp --storage-account $storage --consumption-plan-location "$location" --resource-group $resourceGroup --os-type Linux --runtime python --runtime-version $pythonVersion --functions-version $functionsVersion
+
